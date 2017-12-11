24ef71f24c0d81b6a551413bc016ce66233837bb
Dave Tonge <dave.tonge@momentumft.co.uk>
2017-12-11 06:57:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const cors = require("cors")
+const atob = require("atob")
+const got = require("got")
+const { resolve } = require("url")
+
+const getJWKS = url => {
+  return got(url, { json: true })
+    .then(({ body }) => {
+      if (body.keys) {
+        return body
+      }
+      if (body.jwks_uri) {
+        return getJWKS(body.jwks_uri)
+      }
+      if (url.indexOf("./well-known") !== -1) {
+        return getJWKS(resolve(url, "/.well-known/openid-configuration"))
+      }
+      throw new Error("No Keys found")
+    })
+    .catch(err => {
+      if (err.statusCode === 200 && url.indexOf("./well-known") === -1) {
+        return getJWKS(resolve(url, "/.well-known/openid-configuration"))
+      }
+      throw err
+    })
+}
+
+const app = express()
+app.use(cors())
+
+app.get("/:issuer", (req, res) => {
+  const issuer = atob(req.params.issuer)
+  if (issuer.indexOf("http") !== 0) {
+    return res.status(404).send()
+  }
+  getJWKS(issuer)
+    .then(data => {
+      res.json(data)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(404).send()
+    })
+})
+
+app.listen(3001)
