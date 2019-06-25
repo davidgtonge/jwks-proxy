d494484e58c49296f84a5134c22961fd357d156d
Dave Tonge <dave.tonge@momentumft.co.uk>
2019-06-25 09:02:50 -0400

    Fix resolver

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 'use strict'
 
-// Some jwks endpoints are served with certs signed
-// by industry specific CAs. As this is a debugging
-// utility, such endpoints should be supported
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 const express = require("express")
 const cors = require("cors")
 const atob = require("atob")
 const got = require("got")
-const { resolve } = require("url")
+const URL = require("url")
+const path = require("path")
+
+const addToUrl = (url, trailing) => {
+  const { pathname } = URL.parse(url)
+  return URL.resolve(url, path.join(pathname, trailing))
+}
+
+const hasWellKnown = url => url.indexOf(".well-known") !== -1
 
 const getJWKS = url => {
-  return got(url, { json: true })
+  // Some jwks endpoints are served with certs signed
+  // by industry specific CAs. As this is a debugging
+  // utility, such endpoints should be supported
+  return got(url, { json: true, rejectUnauthorized: false })
     .then(({ body }) => {
       if (body.keys) {
         return body
@@ -20,14 +27,14 @@ const getJWKS = url => {
       if (body.jwks_uri) {
         return getJWKS(body.jwks_uri)
       }
-      if (url.indexOf("./well-known") !== -1) {
-        return getJWKS(resolve(url, "/.well-known/openid-configuration"))
+      if (!hasWellKnown(url)) {
+        return getJWKS(addToUrl(url, "/.well-known/openid-configuration"))
       }
       throw new Error("No Keys found")
     })
     .catch(err => {
-      if (err.statusCode === 200 && url.indexOf("./well-known") === -1) {
-        return getJWKS(resolve(url, "/.well-known/openid-configuration"))
+      if (!hasWellKnown(url)) {
+        return getJWKS(addToUrl(url, "/.well-known/openid-configuration"))
       }
       throw err
     })
